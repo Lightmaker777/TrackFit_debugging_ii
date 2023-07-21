cb351086654e9d08f821defe012bd081f982a244
Ievgeniia Ponomarenko <133200860+Lightmaker777@users.noreply.github.com>
2023-07-21 12:55:15 +0200

    Update log.py

--- log.py
+++ log.py
@@ -5,7 +5,7 @@ def log_activity(activity, calories_burned, num_workouts, duration):
     with open("activity_log.txt", "a") as file:
         file.write(f"Activity: {activity}\n")
         file.write(f"Calories Burned: {calories_burned} calories\n")
-        file.write(f"Number of Workouts: {num_workouts}\n")
+        file.write(f"Number of Workouts: {num_workouts + 1}\n")
         file.write(f"Duration: {duration} minutes\n\n")
 
 
